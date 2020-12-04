10afe34095b816eee4ccf4ba16b60ffac693a57d
Karlo Sintic <karlo.sintic@gmail.com>
2020-12-04 01:21:37 +0100

    Add first day

---
+++ 1/day1.py
@@ -0,0 +1,22 @@
+def find(list):
+    for e in list:
+        if 2020-e in list:
+            return (2020-e)*e
+
+def find2(list):
+    for i, e in enumerate(list):
+        for e2 in list[i+1:]:
+            if 2020-e-e2 in list:
+                return (2020-e-e2)*e*e2
+
+
+
+list = []
+for line in open(r'input.txt', 'r'):
+    list.append(int(line[:-1]))
+
+number=find(list)
+print(number)
+
+number=find2(list)
+print(number)
